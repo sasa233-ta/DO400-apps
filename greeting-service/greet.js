61cf37c1c7823c230e98194112178d7392687c45
Student User <student@workstation.lab.example.com>
2022-09-22 03:20:32 -0400

    Modify greeting message for v3

--- greeting-service/greet.js
+++ greeting-service/greet.js
@@ -1,8 +1,5 @@
 module.exports = function greet(name) {
 
- return `Hello ${name || "guest"} from version 2!`;
+ return `Hello ${name || "guest"} from version 3!`;
 
 }
-
-
-python tools/check.py http://greeting-service-gppkup-deploy-strategies.apps.ap46a.prod.ole.redhat.com
