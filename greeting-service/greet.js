a7f5891134abd4556c398fa6a146dda7bc903bd8
Student User <student@workstation.lab.example.com>
2022-09-22 03:17:49 -0400

    update greet.js

--- greeting-service/greet.js
+++ greeting-service/greet.js
@@ -3,3 +3,6 @@ module.exports = function greet(name) {
  return `Hello ${name || "guest"} from version 2!`;
 
 }
+
+
+python tools/check.py http://greeting-service-gppkup-deploy-strategies.apps.ap46a.prod.ole.redhat.com
